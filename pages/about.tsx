3299b8825a9c1829c08a03587d1231e408475efe
Nicholas Yang <ny585@nyu.edu>
2020-07-31 22:42:35 -0400

    Factored out page layout into separate component and worked on About copy

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,37 +1,79 @@
 import { createUseStyles } from "react-jss";
 import Head from "next/head";
+import PageLayout from "../components/PageLayout";
 
 const useStyles = createUseStyles({
   About: {
-    backgroundColor: "#F3B924",
-    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  orangeSquare: {
+    width: "45vw",
+    height: "45vw",
+    margin: {
+      top: "8vh",
+      bottom: "4vh",
+    },
+    backgroundColor: "#F66717",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  title: {
+    fontSize: "2.5em",
+    fontWeight: 400,
+  },
+  paragraph: {
+    fontSize: "1.1em",
+    lineHeight: "1.3em",
+    maxWidth: "600px",
   },
 });
 export default function About() {
   const classes = useStyles();
   return (
-    <div>
+    <div className="container">
       <Head>
         <title>About | SPARK: A Torch Mentorship Program</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={classes.About}>
-        <h1> About </h1>
-        <style jsx global>{`
-          html,
-          body {
-            padding: 0;
-            margin: 0;
-            font-family: Source Sans Pro, -apple-system, BlinkMacSystemFont,
-              Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-              Helvetica Neue, sans-serif;
-          }
+      <PageLayout>
+        <div className={classes.About}>
+          <div className={classes.orangeSquare}>
+            <h1 className={classes.title}> About </h1>
+            <p className={classes.paragraph}>
+              Spark is a mentorship program run by TorchNYU, an organization
+              dedicated towards improving NYU CS, and BUGS, NYU's open source
+              development club.
+            </p>
+            <p className={classes.paragraph}>
+              We teach students practical and pragmatic software development
+              through working on and shipping projects.
+            </p>
+            <p className={classes.paragraph}>
+              Some of the technologies we use include React, JavaScript, JSS and
+              many others
+            </p>
+          </div>
+        </div>
+      </PageLayout>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: Source Sans Pro, -apple-system, BlinkMacSystemFont,
+            Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+            Helvetica Neue, sans-serif;
+        }
 
-          * {
-            box-sizing: border-box;
-          }
-        `}</style>
-      </main>
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,8 @@
 import Head from "next/head";
 import { createUseStyles } from "react-jss";
-import Link from "next/link";
+import PageLayout from "../components/PageLayout";
 
 const useStyles = createUseStyles({
-  Home: {
-    display: "grid",
-    gridTemplateColumns: "minmax(250px, 1fr) 3fr minmax(250px, 1fr)",
-    backgroundColor: "#F3B924",
-    minHeight: "100vh",
-  },
-  header: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  title: {
-    paddingTop: "max(40px, 10vh)",
-    fontSize: "3em",
-    fontWeight: 600,
-    margin: "0",
-    minWidth: "150px",
-  },
-  subtitle: {
-    fontWeight: 500,
-    fontSize: "2em",
-    margin: "0",
-    maxWidth: "150px",
-  },
   squares: {
     display: "flex",
     flexDirection: "column",
@@ -61,59 +37,17 @@ const useStyles = createUseStyles({
     backgroundColor: "#F0450F",
     marginBottom: "3vh",
   },
-  links: {
-    paddingTop: "max(40px, 10vh)",
-    display: "flex",
-    flexDirection: "column",
-    paddingLeft: "20px",
-  },
-  link: {
-    margin: "10px",
-    fontSize: "2em",
-    fontWeight: 400,
-    "&:hover": {
-      textDecoration: "underline",
-    },
-  },
-  "@media(max-width: 1200px)": {
-    Home: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-    },
-    header: {
-      textAlign: "center",
-    },
-    title: {
-      paddingTop: "20px",
-    },
-    subtitle: {
-      maxWidth: "500px",
-    },
-    link: {
-      textDecoration: "underline",
-    },
-    links: {
-      flexDirection: "row",
-      paddingTop: "20px",
-    },
-  },
 });
 
 export default function Home() {
   const classes = useStyles();
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>SPARK: A Torch Mentorship Program</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={classes.Home}>
-        <div className={classes.header}>
-          <h1 className={classes.title}>SPARK</h1>
-          <h2 className={classes.subtitle}> A Torch Mentorship Program</h2>
-        </div>
+      <PageLayout>
         <div className={classes.squares}>
           <div className={classes.orangeSquare}>
             <div className={classes.darkOrangeSquare}>
@@ -121,19 +55,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className={classes.links}>
-          <Link href="/about">
-            <h3 className={classes.link}>About </h3>
-          </Link>
-          <Link href="/projects">
-            <h3 className={classes.link}>Projects</h3>
-          </Link>
-          <Link href="/join">
-            <h3 className={classes.link}>Join Us</h3>
-          </Link>
-        </div>
-      </main>
-
+      </PageLayout>
       <style jsx global>{`
         html,
         body {
