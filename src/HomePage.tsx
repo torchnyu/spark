b223ec5b7aed4bfb0bffd6b8b85135fc618f7ede
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-08-11 10:09:42 -0400

    minor updates

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,11 +1,16 @@
-import React from "react"
+import React from "react";
 import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
+  "@keyframes fadein": {
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  },
   HomePage: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    animation: "'$fadein' 2s",
   },
   orangeSquare: {
     width: "45vw",
@@ -36,31 +41,60 @@ const useStyles = createUseStyles({
     backgroundColor: "#F0450F",
     marginBottom: "3vh",
   },
-  '@media(max-width: 1000px)': {
+  redRectangle: {
+    width: "55vw",
+    height: "10vw",
+    backgroundColor: "#FF4B3E",
+    marginBottom: "3vh",
+    marginTop: "10vh",
+  },
+  blueRectangle: {
+    width: "55vw",
+    height: "10vw",
+    backgroundColor: "#197278",
+    marginBottom: "3vh",
+  },
+  greenRectangle: {
+    width: "55vw",
+    height: "10vw",
+    backgroundColor: "#74A4BC",
+    marginBottom: "3vh",
+  },
+  "@media(max-width: 1000px)": {
     orangeSquare: {
       width: "70vw",
-      height: "70vw"
+      height: "70vw",
     },
     darkOrangeSquare: {
       width: "52vw",
-      height: "52vw"
+      height: "52vw",
     },
     redSquare: {
       width: "35vw",
-      height: "35vw"
-    }
-  }
+      height: "35vw",
+    },
+  },
 });
 
 export default function HomePage() {
   const classes = useStyles();
+
+  const randNumber = Math.floor(Math.random() * 2 + 1);
   return (
-        <div className={classes.HomePage}>
-          <div className={classes.orangeSquare}>
-            <div className={classes.darkOrangeSquare}>
-              <div className={classes.redSquare} />
-            </div>
+    <div className={classes.HomePage}>
+      {randNumber % 2 === 0 ? (
+        <div>
+          <div className={classes.redRectangle} />
+          <div className={classes.greenRectangle} />
+          <div className={classes.blueRectangle} />
+        </div>
+      ) : (
+        <div className={classes.orangeSquare}>
+          <div className={classes.darkOrangeSquare}>
+            <div className={classes.redSquare} />
           </div>
         </div>
+      )}
+    </div>
   );
 }
