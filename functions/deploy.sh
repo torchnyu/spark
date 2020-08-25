7df321a37661a6c8d8d44472f90304d37c859c38
Nicholas Yang <ny585@nyu.edu>
2020-08-25 18:18:48 -0400

    Deleted functions since we don't need it anymore. Made JoinUs link to a Google Form

--- functions/deploy.sh
+++
@@ -1,2 +0,0 @@
-zip -r function.zip .
-aws lambda update-function-code --function-name SendSparkEmail  --zip-file fileb://function.zip 
--- src/JoinUsPage.tsx
+++ src/JoinUsPage.tsx
@@ -1,11 +1,5 @@
-import React, { useState } from "react";
+import React  from "react";
 import { createUseStyles } from "react-jss";
-import TextField from "@material-ui/core/TextField";
-import Box from "@material-ui/core/Box";
-import { Button } from "@material-ui/core";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
-import FormControl from "@material-ui/core/FormControl";
 
 const useStyles = createUseStyles({
   JoinUsPage: {
@@ -48,82 +42,17 @@ const useStyles = createUseStyles({
   },
 });
 
-const API_URL =
-  "https://ywhk4gbxs1.execute-api.us-east-1.amazonaws.com/Prod/email";
 
 const JoinUsPage = () => {
   const classes = useStyles();
-  /*const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [role, setRole] = useState("mentee");
-  const [github, setGitHub] = useState("");
-  const [error, setError] = useState("");
-  const [message, setMessage] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  async function handleSubmit(event: React.FormEvent) {
-    event.preventDefault();
-    if (email === "" || name === "") {
-      setError("Email and name cannot be empty");
-      return;
-    }
-    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-      setError("Email must be valid");
-      return;
-    }
-    try {
-      setIsSubmitting(true);
-      await fetch(API_URL, {
-        method: "POST",
-        body: JSON.stringify({ name, email, github, role }),
-      });
-      setError("");
-      setMessage("Successfully submitted. We'll be in touch");
-    } catch (e) {
-      console.error(e);
-      setError("Failed to submit. Try again?");
-    }
-    setIsSubmitting(false);
-  }*/
   return (
     <div className={classes.JoinUsPage}>
       <div className={classes.container}>
         <h1> Join Us </h1>
         <p>
-          We're happy that you're interested in joining us! Please email
-          nick@nicholasyang.com for more details
+          We're happy that you're interested in joining us! Please fill out
+          <a href={"https://docs.google.com/forms/d/e/1FAIpQLSdKjX0bLjmz9c_2X4NrZKbmZ1hTkfDs3QYO4ycYBrSY19-gRA/viewform?usp=sf_link"}> this form </a> and we'll be in touch
         </p>
-        {/*<form onSubmit={handleSubmit} className={classes.form}>
-      <p>
-        We're happy that you're interested in joining us! Please fill out the following form
-        and we'll be in touch
-      </p>
-      <Box m={1}><TextField disabled={isSubmitting} value={email} onChange={e => setEmail(e.currentTarget.value)} label="Email" variant="outlined" /></Box>
-      <Box m={1}><TextField disabled={isSubmitting} value={name} onChange={e => setName(e.currentTarget.value)} label="Name" variant="outlined" /></Box>
-      <p>
-        Mentor or mentee?
-      </p>
-      <Box m={1}>
-        <FormControl variant="outlined">
-          <Select disabled={isSubmitting} value={role} onChange={(e) => {
-            console.log(e.target.value);
-            setRole(e.target.value as string)
-          }}>
-            <MenuItem value={"mentee"}> Mentee </MenuItem>
-            <MenuItem value={"mentor"}> Mentor </MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <p>
-        If you have a GitHub account, please provide your username so
-        that we can add you to the Torch organization
-      </p>
-      <Box m={1}><TextField disabled={isSubmitting} value={github} onChange={e => setGitHub(e.currentTarget.value)} label="GitHub (optional)" variant="outlined" /></Box>
-      {error !== "" && <div className={classes.error}> {error} </div>}
-      {message !== "" && <div className={classes.message}> {message} </div>}
-      <Box m={1}>
-        <Button disabled={isSubmitting} variant="contained" color="primary" type="submit"> Submit </Button>
-      </Box>
-    </form>*/}
       </div>
     </div>
   );
